c39bc9e8a4e8d29831cb703f90d992e0d11dcefb
Arley HR <arley.hr@hotmail.com>
2018-10-06 09:54:13 -0500

    hello world with HapiJS

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const Hapi = require('hapi')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const server = Hapi.server({
+  port: process.env.PORT || 3000,
+  host: 'localhost'
+})
+
+async function init() {
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (req, h) => 'hello world!'
+  })
+
+  try {
+    await server.start()
+  } catch (e) {
+    console.log(e)
+    process.exit(1)
+  }
+
+  console.log(`Server listening: ${server.info.uri}`)
+}
+
+init()
